56f93e48656a54eff9b6ac4acbda4e264c416ea1
Morgan Fudge <mfudge92@gmail.com>
2017-11-02 18:35:18 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  
 end
 
 def words_five_letters_long(text)
-  value = text.scan(/(^[a-z][a-z][a-z][a-z][a-z]$)/i)
+  value = text.scan(/\w{1,10}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
