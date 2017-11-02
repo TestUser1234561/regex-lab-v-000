ff063d1814dc8279ba9ef79bd8cb173d849a2a66
Morgan Fudge <mfudge92@gmail.com>
2017-11-02 18:28:39 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.match(/(?:un){2,3}|ing/i)
-  	return true
-  else
-  	return false
-  end
+
 end
 
 def words_five_letters_long(text)
